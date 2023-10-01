e0945d140a1f4824d78654bfa4333947674aadf3
farah hezma <fhezma123@gmail.com>
2023-10-01 14:48:47 +0300

    Story Book

--- config/upload.js
+++ config/upload.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 const path = require('path');
 //Set storge engine for an profile image
 const storage=multer.diskStorage({
-  destination:'./public/uploads/',
+  destination:'https://github.com/fara77ezma/StoryBook/tree/master/public/uploads',
   filename:(req,file,cb)=>{
     cb(null,file.fieldname+'-'+Date.now()+path.extname(file.originalname));
   }
